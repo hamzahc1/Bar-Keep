1832ca84067b92a0801c3accfde04d9933de9c84
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-02-15 15:56:00 -0800

    Added files for server hosting with express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-//server
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.use('/client', express.static( __dirname + '/client'));
+
+app.route('/').
+  options(function (req, res, next) {
+      res.status(200).end();
+      next();
+  }).
+  get(function (req, res) {
+  	res.sendFile(path.join(__dirname.split("/").slice(0,-1).join('/') + '/index.html'));
+  });
+
+
+var port = 8080;
+app.listen(8080, function(){
+console.log("Server listening on....", port);
+});
+
+// module.exports = app;
